3b42dc3afcf1a658de60893e986612926800c3de
Tomasz Jankowski <tomasz.jankowski.mail@gmail.com>
2018-02-03 17:15:16 +0100

    Disable matplotlib's GUI in UTs __init__.py

--- python/tests/__init__.py
+++ python/tests/__init__.py
@@ -12,3 +12,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see http:#www.gnu.org/licenses/.
 #
+
+# There's no need to display interactive plots
+import matplotlib
+matplotlib.use('AGG')
