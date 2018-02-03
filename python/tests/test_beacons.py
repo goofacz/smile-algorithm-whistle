4e39d8c39b2e782be8641aedf73a36eb1fe5017d
Tomasz Jankowski <tomasz.jankowski.mail@gmail.com>
2018-02-03 22:56:46 +0100

    Add whistle.beacons

---
+++ python/tests/test_beacons.py
@@ -0,0 +1,36 @@
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see http:#www.gnu.org/licenses/.
+#
+
+import unittest
+from io import StringIO
+import numpy as np
+from whistle.beacons import Beacons
+
+
+class TestBeacons(unittest.TestCase):
+    def test_load_csv(self):
+        content = StringIO(
+            "12345678,RX,105000927418,205000927418,37.500000,87.500000,79.000000,305010927418,405010927418,137.500000,127.500000,46.000000,17592186044420,281474976710655,1,0\n"
+            "98765432,TX,140001177591,140001177591,37.500000,37.500000,0.000000,140011177591,140011177591,37.500000,37.500000,0.000000,17592186044417,281474976710655,2,1")
+
+        # Check access to new fields
+        nodes = Beacons.load_csv(content)
+        self.assertTupleEqual((2, 16), nodes.shape)
+
+        np.testing.assert_equal((0, 1), nodes[:, Beacons.IS_ECHO])
+
+
+if __name__ == '__main__':
+    unittest.main()
