e7d9978e548b55c43e09fc185f1b0fe27dba4f7b
Tomasz Jankowski <tomasz.jankowski.mail@gmail.com>
2018-02-04 21:45:11 +0100

    Implement Whistle algorithm

--- python/whistle/algorithm.py
+++ python/whistle/algorithm.py
@@ -16,13 +16,12 @@
 import numpy as np
 import scipy.constants as scc
 
+from itertools import combinations
 from whistle.beacons import Beacons
 from whistle.anchors import Anchors
 from smile.results import Results
 
-# Algorithm based on:
-# S. Van Doan and J. Vesely, "The effectivity comparison of TDOA analytical solution methods,"
-# 2015 16th International Radar Symposium (IRS), Dresden, 2015, pp. 800-805.
+
 def _tdoa_analytical(coordinates, distances):
     """
     S. Van Doan and J. Vesely, "The effectivity comparison of TDOA analytical solution methods,"
@@ -61,8 +60,8 @@ def localize_mobile(mac_address, anchors, all_anchors_beacons, all_mobiles_beaco
 
     base_anchor = anchors[anchors[:, Anchors.BASE_ANCHOR] == True, :]
     assert(base_anchor.shape[0] == 1)
-    non_base_anchor = anchors[anchors[:, Anchors.BASE_ANCHOR] == False, :]
-    assert(non_base_anchor.shape[0] >= 2)
+    non_base_anchors = anchors[anchors[:, Anchors.BASE_ANCHOR] == False, :]
+    assert(non_base_anchors.shape[0] >= 3)
 
     mobile_beacons = all_mobiles_beacons[all_mobiles_beacons[:, Beacons.NODE_MAC_ADDRESS] == mac_address]
     anchors_beacons = all_anchors_beacons[all_anchors_beacons[:, Beacons.ORIGIN_NODE_MAC_ADDRESS] == mac_address]
@@ -71,8 +70,11 @@ def localize_mobile(mac_address, anchors, all_anchors_beacons, all_mobiles_beaco
     results = Results.create_array(sequence_numbers.size, position_dimensions=2)
 
     for sequence_number in sequence_numbers:
-        current_anchors_beacons_condition = anchors_beacons[:, Beacons.SEQUENCE_NUMBER] == sequence_number
-        current_anchors_beacons = anchors_beacons[current_anchors_beacons_condition, :]
+        sequence_number_condition = anchors_beacons[:, Beacons.SEQUENCE_NUMBER] == sequence_number
+        current_anchors_beacons = anchors_beacons[sequence_number_condition, :]
+
+        tx_condition = current_anchors_beacons[:, Beacons.DIRECTION] == hash('TX')
+        rx_condition = current_anchors_beacons[:, Beacons.DIRECTION] == hash('RX')
 
         echo_condition = current_anchors_beacons[:, Beacons.IS_ECHO] == 1
         original_condition = np.logical_not(echo_condition)
@@ -80,31 +82,47 @@ def localize_mobile(mac_address, anchors, all_anchors_beacons, all_mobiles_beaco
         non_base_anchor_condition = np.logical_not(base_anchor_condition)
 
         base_echo_condition = np.logical_and(base_anchor_condition, echo_condition)
+        base_echo_tx_condition = np.logical_and(base_echo_condition, tx_condition)
+
         base_original_condition = np.logical_and(base_anchor_condition, original_condition)
+        base_original_rx_condition = np.logical_and(base_original_condition, rx_condition)
+
         non_base_echo_condition = np.logical_and(non_base_anchor_condition, echo_condition)
+        non_base_echo_rx_condition = np.logical_and(non_base_echo_condition, rx_condition)
+
         non_base_original_condition = np.logical_and(non_base_anchor_condition, original_condition)
+        non_base_original_rx_condition = np.logical_and(non_base_original_condition, rx_condition)
 
-        base_echo_beacons = current_anchors_beacons[base_echo_condition, :]
-        base_original_beacons = current_anchors_beacons[base_original_condition, :]
-        non_base_echo_beacons = current_anchors_beacons[non_base_echo_condition, :]
-        non_base_original_beacons = current_anchors_beacons[non_base_original_condition, :]
+        base_echo_tx_beacons = current_anchors_beacons[base_echo_tx_condition, :]
+        base_original_rx_beacons = current_anchors_beacons[base_original_rx_condition, :]
 
-        tD2S = np.zeros(2)
-        for i in (0, 1):
-            distance = np.abs(np.linalg.norm(base_anchor[0, Anchors.POSITION_2D] - non_base_anchor[i, Anchors.POSITION_2D]))
-            tA2S = base_echo_beacons[0, Beacons.BEGIN_CLOCK_TIMESTAMP] - base_original_beacons[0, Beacons.BEGIN_CLOCK_TIMESTAMP]
-            tB2S = non_base_echo_beacons[i, Beacons.BEGIN_CLOCK_TIMESTAMP] - non_base_original_beacons[i, Beacons.BEGIN_CLOCK_TIMESTAMP]
-            tD2S[i] = distance / c - (tB2S - tA2S)
+        non_base_echo_rx_beacons = current_anchors_beacons[non_base_echo_rx_condition, :]
+        non_base_original_rx_beacons = current_anchors_beacons[non_base_original_rx_condition, :]
 
-        coordinates = np.zeros((3, 2))
-        coordinates[0, :] = base_anchor[0, Anchors.POSITION_2D]
-        coordinates[1, :] = non_base_anchor[0, Anchors.POSITION_2D]
-        coordinates[2, :] = non_base_anchor[1, Anchors.POSITION_2D]
+        non_base_echo_rx_beacons_order = np.argsort(non_base_echo_rx_beacons[:, Beacons.NODE_MAC_ADDRESS])
+        non_base_echo_rx_beacons = non_base_echo_rx_beacons[non_base_echo_rx_beacons_order, :]
 
-        timestamps = np.array((float('nan'), tD2S[0], tD2S[1]))
-        distances = timestamps * c
-        position = _tdoa_analytical(coordinates, distances)
+        non_base_original_rx_beacons_order = np.argsort(non_base_original_rx_beacons[:, Beacons.NODE_MAC_ADDRESS])
+        non_base_original_rx_beacons = non_base_original_rx_beacons[non_base_original_rx_beacons_order, :]
 
-        pass
+        positions = []
+        for non_base_anchors_indices in combinations((0, 1, 2), 2):
+            tD2S = []
+            coordinates = [np.array(base_anchor[0, Anchors.POSITION_2D])]
 
+            for i in non_base_anchors_indices:
+                distance = np.abs(np.linalg.norm(base_anchor[0, Anchors.POSITION_2D] - non_base_anchors[i, Anchors.POSITION_2D]))
+                tA2S = base_echo_tx_beacons[0, Beacons.BEGIN_CLOCK_TIMESTAMP] - base_original_rx_beacons[0, Beacons.BEGIN_CLOCK_TIMESTAMP]
+                tB2S = non_base_echo_rx_beacons[i, Beacons.BEGIN_CLOCK_TIMESTAMP] - non_base_original_rx_beacons[i, Beacons.BEGIN_CLOCK_TIMESTAMP]
+                tD2S = np.append(tD2S, distance / c - (tB2S - tA2S))
+
+                coordinates.append(non_base_echo_rx_beacons[i, Beacons.BEGIN_TRUE_POSITION_2D])
+
+            distances = np.array((float('nan'), tD2S[0], tD2S[1])) * c
+            coordinates = np.array(coordinates)
+            positions.append(_tdoa_analytical(coordinates, distances))
+
+            pass
+
+    # TODO Fill results
     return results
